187ea71b4fc04547d7e3cf5abda34ec4e09c4fce
YEONA <79030514+camellia785@users.noreply.github.com>
2024-06-26 19:33:20 +0900

    Title: 짝수만 출력 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240626/짝수만 출력 2/output-only-even-2.py
+++ 240626/짝수만 출력 2/output-only-even-2.py
@@ -1,7 +1,20 @@
-a, b = map(int, input().split())
-i = a
+# a, b = map(int, input().split())
+# i = a
 
-while i >= b:
-    if i % 2 ==0:
-        print(i, end=' ')
-    i-=1
+# while i >= b:
+#     if i % 2 ==0:
+#         print(i, end=' ')
+#     i-=1
+
+
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+b = int(arr[0])
+a = int(arr[1])
+i = b
+
+# 출력
+while i >= a:
+	print(i, end=" ")
+	i -= 2
