98897f4a09a831ddc20c8c862db4b5351f308d38
YEONA <79030514+camellia785@users.noreply.github.com>
2024-06-27 20:16:42 +0900

    Title: 청소당번 정하기, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240627/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+n_class = 0
+n_hallway = 0
+n_toliet = 0
+
+for i in range(1,n):
+    if i%12==0:
+        n_toliet += 1
+
+    elif i%3==0:
+        n_hallway += 1
+    
+    elif i%2==0:
+        n_class += 1
+
+print(f'{n_class} {n_hallway} {n_toliet}')
