1ffb0c96a23027fc0c0366c1769c491d653cf5be
YEONA <79030514+camellia785@users.noreply.github.com>
2024-07-27 18:43:28 +0900

    Title: 문자 개수 세기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/문자 개수 세기/count-char.py
+++ 240727/문자 개수 세기/count-char.py
@@ -10,3 +10,22 @@ for i in range(k):
         count += 1
 
 print(count)
+
+'''
+# 해설 풀이
+
+# 공백을 포함한 문자열을 입력받습니다.
+string = input()
+
+# 소문자 알파벳을 입력받습니다.
+a = input()
+cnt = 0
+
+# 문자열에서 주어진 알파벳이 몇번 나오는지 확인합니다.
+for elem in string:
+	if elem == a:
+		cnt += 1
+
+# 주어진 알파벳이 나온 횟수를 출력합니다.
+print(cnt)
+'''
