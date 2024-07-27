3e212b94a48651d9c621bc3234818dc0cd536595
YEONA <79030514+camellia785@users.noreply.github.com>
2024-07-27 17:50:34 +0900

    Title: a의 b승, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/a의 b승/a-to-the-power-of-b.py
+++ 240727/a의 b승/a-to-the-power-of-b.py
@@ -1,3 +1,4 @@
+# 변수 선언 및 입력
 a, b = map(int, input().split())
 prod = 1
 
@@ -5,3 +6,19 @@ for i in range(b):
     prod*=a
 
 print(prod)
+
+'''
+# 변수 선언 및 입력 방법2
+1) inp를 한번에 받음 array 형태
+inp = input()
+
+2) array형태로 빈칸으로 쪼개고
+arr = inp.split()
+
+3) 0번이 a, 1번이 b
+a = int(arr[0])
+b = int(arr[1])
+
+prod = 1
+
+'''
