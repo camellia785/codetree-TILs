a5a756ded09dce195277493d835a97db358ac73e
YEONA <79030514+camellia785@users.noreply.github.com>
2024-07-27 17:39:39 +0900

    Title: 입력받은 수의 합과 평균, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.py
+++ 240727/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.py
@@ -1,7 +1,7 @@
 n = int(input())
 sum = 0
 
-
+# 합을 구함
 for _ in range(n):
     number = int(input())
     sum += number
@@ -11,3 +11,6 @@ avg = sum/n
 avg_rounded = round(avg, 1)
 
 print(sum, avg_rounded)
+
+# print에서 1째짜리만 구하고 싶으면 다음과 같이 작성
+# print(f"{sum_val} {avg:.1f}")
