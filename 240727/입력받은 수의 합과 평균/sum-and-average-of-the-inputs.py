ffde424d62ac38f05d70b29f643d16266355a2e7
YEONA <79030514+camellia785@users.noreply.github.com>
2024-07-27 17:37:07 +0900

    Title: 입력받은 수의 합과 평균, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.py
+++ 240727/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.py
@@ -6,6 +6,8 @@ for _ in range(n):
     number = int(input())
     sum += number
 
+# 소수점 첫째 자리까지 출력하기 위해서는 round() 함수 사용!! 
 avg = sum/n
+avg_rounded = round(avg, 1)
 
-print(sum, avg)
+print(sum, avg_rounded)
