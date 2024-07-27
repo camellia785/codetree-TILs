0f30b1170a247d9a22b20a4e5c6d48f648c37c11
YEONA <79030514+camellia785@users.noreply.github.com>
2024-07-27 18:13:16 +0900

    Title: 조건에 부합하는 수, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240727/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 240727/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -3,5 +3,6 @@ a = int(input())
 for i in range(1,a+1):
     if (i%2==0 and i%4!=0) or ((i//8)%2==0 ) or (i%7<4):
         continue
-    else:
-        print(i, end=' ')
+        
+    # 원래는 else 쓰는데, continue 활용 문제니까 continue 하면 바로 print 가능
+    print(i, end=' ')
