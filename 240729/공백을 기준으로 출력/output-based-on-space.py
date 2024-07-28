e6fbd880ff3b79c7b9b0212477e030f95ba05c42
YEONA <79030514+camellia785@users.noreply.github.com>
2024-07-29 00:50:06 +0900

    Title: 공백을 기준으로 출력, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/공백을 기준으로 출력/output-based-on-space.py
+++ 240729/공백을 기준으로 출력/output-based-on-space.py
@@ -7,3 +7,14 @@ st2_no_space = st2.replace(" ", "")
 
 result = st1_no_space + st2_no_space
 print(result)
+
+'''
+# 문자열을 전부 순회하며 공백을 제외한 모든 문자를 출력하는 방법
+for elem in string:
+	if elem != " ":
+		print(elem, end="")
+
+for elem in string2:
+	if elem != " ":
+		print(elem, end="")
+'''
