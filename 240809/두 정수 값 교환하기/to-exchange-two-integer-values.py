4a4dbcd2a70b38ad6b18b6a38528203cbcccdc84
YEONA <79030514+camellia785@users.noreply.github.com>
2024-08-09 18:02:17 +0900

    Title: 두 정수 값 교환하기, Time: 227ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/두 정수 값 교환하기/to-exchange-two-integer-values.py
+++ 240809/두 정수 값 교환하기/to-exchange-two-integer-values.py
@@ -1,3 +1,4 @@
+# (1) change라는 함수를 이용해서 불러오기
 n, m = map(int, (input().split()))
 
 def change(n,m):
@@ -5,3 +6,19 @@ def change(n,m):
     print(n,m)
 
 change(n,m)
+
+'''
+# (2) tuple 형태로 받고 실행
+
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+
+
+def swap(n, m):
+    n, m = m, n
+    return n, m
+
+
+n, m = swap(n, m)
+print(n, m)
+'''
