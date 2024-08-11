3ddfda8665f2fa8c8237e7f247160550f4c577f0
YEONA <79030514+camellia785@users.noreply.github.com>
2024-08-11 23:59:56 +0900

    Title: 반복 출력하기 2, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/반복 출력하기 2/repeated-output-2.py
+++ 240811/반복 출력하기 2/repeated-output-2.py
@@ -1,3 +1,4 @@
+#for문사용
 def print_stars(n):
     for _ in range(n):
         print("HelloWorld")
@@ -5,3 +6,22 @@ def print_stars(n):
 n = int(input())
 
 print_stars(n)
+
+'''
+#재귀함수 사용
+
+# 변수 선언 및 입력:
+n = int(input())
+
+
+def print_string(n): # 1부터 n번째 줄까지 문자열을 출력하는 함수
+    if n == 0:       # n이 0이라면, 더 이상 진행하지 않고
+        return       # 퇴각합니다.
+
+    print_string(n - 1)   # 1부터 n - 1번째 줄까지 출력하는 함수
+    print("HelloWorld")
+    
+
+print_string(n)
+
+'''
