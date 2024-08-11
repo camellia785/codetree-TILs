0b79a008d0e3238c1c659a14271bea1047b9a19a
YEONA <79030514+camellia785@users.noreply.github.com>
2024-08-11 21:08:15 +0900

    Title: 짝수만 2로 나누기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/짝수만 2로 나누기/divide-even-numbers-by-2.py
+++ 240811/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -13,3 +13,22 @@ def magic_function(N, N_list):
     print(" ".join(map(str, N_list)))  
 
 magic_function(N, N_list)
+
+'''
+# 변수 선언 및 입력:
+n = int(input())
+arr = list(map(int, input().split()))
+
+
+def modify(arr):
+    for i in range(n):
+        if arr[i] % 2 == 0:
+            arr[i] //= 2
+
+
+modify(arr)
+for elem in arr:
+    print(elem, end=" ")
+
+
+'''
