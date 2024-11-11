96958c1cdf7e64b5556d1997c70a8d5b20388e55
YEONA <79030514+camellia785@users.noreply.github.com>
2024-11-11 18:26:37 +0900

    Title: 1차원 젠가, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241111/1차원 젠가/jenga-1d.py
+++ 241111/1차원 젠가/jenga-1d.py
@@ -1,18 +1,11 @@
-N=int(input())
-A=[int(input()) for _ in range(N)]
-
-# arr배열의 arr[s] ~ arr[e]제거하고, 남은 결과를 리턴
+N = int(input())
+A = [int(input()) for _ in range(N)]
 
 def f(arr, s, e) -> list[int]:
-    temp = []
-    for i in range(len(arr)):
-        if s <= i <= e:
-            continue
-        temp.append[arr[i]]
-    return temp
+    return [arr[i] for i in range(len(arr)) if i < s or i > e]
 
 for _ in range(2):
-    s, e = map(int, input().split()) # 1
+    s, e = map(int, input().split())
     A = f(A, s-1, e-1)
 
 print(len(A))
