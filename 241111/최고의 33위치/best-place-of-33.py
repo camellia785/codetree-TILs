389631a316d51f4b6f163b5f913115f96f4854dc
YEONA <79030514+camellia785@users.noreply.github.com>
2024-11-11 17:32:31 +0900

    Title: 최고의 33위치, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 241111/최고의 33위치/best-place-of-33.py
+++ 241111/최고의 33위치/best-place-of-33.py
@@ -6,7 +6,7 @@ for row in range(1, N-1): # 행마다
     for col in range(1, N-1): #열마다
         cnt = 0
         for r in range(row-1, row+2):
-            for c in rnage(col-1, col+2):
+            for c in range(col-1, col+2):
                 cnt += A[r][c]
             answer = max(answer, cnt)
 
