1c87811c8bc3b0649ce3f861b9ecf46aed72f7f0
YEONA <79030514+camellia785@users.noreply.github.com>
2024-11-11 17:31:43 +0900

    Title: 최고의 33위치, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241111/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,14 @@
+N = int(input())
+A =[list(map(int, input().split())) for _ in range(N)]
+
+answer = 0
+for row in range(1, N-1): # 행마다
+    for col in range(1, N-1): #열마다
+        cnt = 0
+        for r in range(row-1, row+2):
+            for c in rnage(col-1, col+2):
+                cnt += A[r][c]
+            answer = max(answer, cnt)
+
+
+print(answer)
